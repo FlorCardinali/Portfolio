32f1d33920df6e567922933fc8b9683cd69d7371
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2024-01-21 19:44:43 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      colors :{
+        primary: "#be8c58",
+        bg: "#2f2d2d",
+        contrast: "#d1cfcd"
+      }
+    },
+  },
+  plugins: [],
+}
+
