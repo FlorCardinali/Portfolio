c7f07cdb26b43f941be715e53dcd01b1bbf8b021
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2024-02-21 08:13:01 -0300

    ortografia

--- vite/src/components/AboutMe.jsx
+++ vite/src/components/AboutMe.jsx
@@ -17,9 +17,9 @@ const AboutMe = () => {
                         <p id="about-text">
                         Me presento, yo soy <span className={spam_style}>Florencia Cardinali</span>, Estoy por terminar una <span className={spam_style}>Tecnicatura Universitaria en Programación</span> en la Universidad Tecnológica Nacional. 
                         Soy <span className={spam_style}>perfeccionista</span> y <span className={spam_style}>adaptable</span>, me agrada trabajar en equipos donde se valore y se fomente el crecimiento personal y <span className={spam_style}>profesional</span>. 
-                        Me gusta centrarme en la calidad de los proyectos, en la <span className={spam_style}>legibilidad del codigo</span> y su limpieza, basciamente, si puedo dejar el codigo mas limpio que como lo encontre, mejor.
-                        Los ambitos en donde mas me he desarrollado son; el  <span className={spam_style}>desarrollo de videojuegos</span> en Unity y Godot, y el <span className={spam_style}>desarrollo web </span> tanto frontend como backend.
-                        Como se observa a simple vista, soy albina, tengo una discapacidad visual parcial del 78%, sin embargo, esto no me limita en mi carrera y pienso seguir creciendo y  <span className={spam_style}>estudiando siempre</span>. 
+                        Me gusta centrarme en la calidad de los proyectos, en la <span className={spam_style}>legibilidad del codigo</span> y su limpieza, basciamente, si puedo dejar el código mas limpio que como lo encontre, mejor.
+                        Los ámbitos en donde más me he desarrollado son el  <span className={spam_style}>desarrollo de videojuegos</span> en Unity y Godot, y el <span className={spam_style}>desarrollo web </span> tanto frontend como backend.
+                        Tengo una discapacidad visual parcial del 76%, sin embargo, esto no me limita en mi carrera y pienso seguir creciendo y <span className={spam_style}>estudiando siempre</span>. 
                         </p>
                     </article>
                     <div className="rounded-3xl  min-w-[300px] min-h-[400px] max-h-[500px] bg-cover bg-center bg-no-repeat bg-[url('/foto-about.webp')] shadow-xl">               
